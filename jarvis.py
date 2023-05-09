05c4aa8113da6b5ea7079054d35a2eec8261a1a3
akhand769 <akhand769@gmail.com>
2023-05-10 01:59:39 +0530

    part 1

--- jarvis.py
+++ jarvis.py
@@ -10,7 +10,7 @@ print(voices)
 
 def speak(audio):
     engine.setProperty('rate', 180)
-    engine.setProperty('voice',voices[1].id)
+    engine.setProperty('voice',voices[0].id)
     engine.say(audio)
     engine.runAndWait()
 def wishme():
@@ -19,18 +19,18 @@ def wishme():
         speak("Good Evening")
     else:
         speak("Good Morning")
-    speak("I am Tokyo Sir")
-    speak("Please Tell me How may I help You")
+    speak("I am Max")
+    speak("Please Tell me How may I help You Sir")
 def takeCommand():
     '''
     '''
     r= sr.Recognizer()
     with sr.Microphone() as source:
-        speak("I am Listening")
-        r.pause_threshold = 1
+        r.pause_threshold = 0.1
+        speak("I am listening...")
         audio = r.listen(source)
     try:
-        speak("Recognizing")
+        speak("Recognizing...")
         query = r.recognize_google(audio,language='en-in')
         print(f"User said: {query}\n")
     except Exception as e :
@@ -41,21 +41,37 @@ def takeCommand():
 if __name__=="__main__":
     wishme()
     while True:
-        query = takeCommand().lower()
-        if 'stop' in query:
-            break
-        elif 'wikipedia' in query:
-            speak("Searching Wikipedia....")
-            query = query.replace("wikipedia","")
-            results = wikipedia.summary(query,sentences=2)
-            speak("According to wikipedia")
-            speak(results)
-        elif 'open youtube' in query:
-            chrome_path="C:/Program Files/Google/Chrome/Application/chrome.exe"
-            wb.get(chrome_path)
-            wb.open("youtube.com")
-        elif 'play music' in query:
-            music_dir=
+        r=sr.Recognizer()
+        query2='Hello'
+        with sr.Microphone() as source:
+            r.pause_threshold = 0.1
+            audio = r.listen(source)
+        try:
+            query2 = r.recognize_google(audio,language='en-in')
+        except Exception as e :
+            print(e)
+        if query2=='Max':
+            query = takeCommand().lower()
+            if 'stop' in query:
+                break
+            elif 'wikipedia' in query:
+                speak("Searching Wikipedia....")
+                query = query.replace("wikipedia","")
+                results = wikipedia.summary(query,sentences=2)
+                speak("According to wikipedia")
+                speak(results)
+            elif 'open youtube' in query:
+                # chrome_path="C:/Program Files/Google/Chrome/Application/chrome.exe"
+                # wb.get(chrome_path)
+                wb.open("youtube.com")
+            elif 'play music' in query:
+                music_dir='C:\\Users\\akhan\\Music'
+                songs = os.listdir(music_dir)
+                os.startfile(os.path.join(music_dir,songs[0]))
+            elif 'current time' in query:
+                strTime = datetime.datetime.now().strftime("%H:%M:%S")
+                speak(strTime)
+        
 
 
 
